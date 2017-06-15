5820c4f7392e9fba6ba6e35e96142ac1c43d88cc
John Fulton <john@john-fulton.com>
2017-06-16 00:05:42 +1000

    Fix build agent docker build and update vault reading to use -field

--- dev/build-agent.tf
+++ dev/build-agent.tf
@@ -2,6 +2,7 @@ variable "pub_key" {}
 variable "pvt_key" {}
 variable "ssh_fingerprint" {}
 variable "agent_token" {}
+variable "bitbucket_rsa" {}
 
 resource "digitalocean_droplet" "build-agent" {
   image = "coreos-stable"
@@ -13,17 +14,24 @@ resource "digitalocean_droplet" "build-agent" {
     "${var.ssh_fingerprint}"
   ]
 
+  connection {
+    user = "core"
+    type = "ssh"
+    private_key = "${file("${var.pvt_key}")}"
+    timeout = "2m"
+  }
+
+  provisioner "file" {
+    content = "${var.bitbucket_rsa}"
+    destination = "/home/core/.ssh/id_rsa"
+  }
 
   provisioner "remote-exec" {
     inline = [
-      "docker run -d -e BUILDKITE_AGENT_TOKEN='${var.agent_token}' buildkite/agent"
+      "cd",
+      "sudo mkdir /build",
+      "sudo chmod a+w /build",
+      "docker run -d -v /var/run/docker.sock:/var/run/docker.sock -v $HOME/.ssh:/root/.ssh -v /build:/build -e BUILDKITE_BUILD_PATH='/build' -e BUILDKITE_AGENT_TOKEN='${var.agent_token}' buildkite/agent"
     ]
-
-    connection {
-      user = "core"
-      type = "ssh"
-      private_key = "${file("${var.pvt_key}")}"
-      timeout = "2m"
-    }
   }
 }
--- terraform.sh
+++ terraform.sh
@@ -2,25 +2,25 @@ PUB_KEY="id_rsa.pub"
 PRIV_KEY="id_rsa"
 SSH_PRINT=$(ssh-keygen -l -f $HOME/.ssh/$PUB_KEY -E md5 | awk '{print $2}' | sed 's/^MD5://')
 DOCKER_RUN="docker run -it -w /data -v $PWD/$1:/data -v ${HOME}/.ssh:/ssh/ hashicorp/terraform"
-VAULT_READ="vault read --format=json"
-JQ="jq -r .data.value"
-
+VAULT_READ="vault read -field=value"
 #### VAULT SECRETS
 # Assumes you have logged into vault and have access to the secrets
 
 echo "Getting secrets from vault..."
 echo "Getting digitalocean token..."
-DO_TOKEN=$( $VAULT_READ secret/terraform_digital_ocean_api_key | $JQ )
+DO_TOKEN=$( $VAULT_READ secret/terraform_digital_ocean_api_key )
 echo "Getting buildkite agent token..."
-BUILDKITE_AGENT_TOKEN=$( $VAULT_READ secret/buildkite_agent_token | $JQ )
+BUILDKITE_AGENT_TOKEN=$( $VAULT_READ secret/buildkite_agent_token )
 echo "Getting registry google cloud storage token..."
-REGISTRY_GCS_TOKEN=$( $VAULT_READ secret/registry_gcs_key | $JQ )
-echo "Getting registry htpasswd"
-REGISTRY_HTPASSWD=$( $VAULT_READ secret/registry_htpassw | $JQ )
+REGISTRY_GCS_TOKEN=$( $VAULT_READ secret/registry_gcs_key )
+echo "Getting registry htpasswd..."
+REGISTRY_HTPASSWD=$( $VAULT_READ secret/registry_htpasswd )
 echo "Getting dnsimple api token..."
-DNSIMPLE_TOKEN=$( $VAULT_READ secret/dnsimple_token | $JQ )
+DNSIMPLE_TOKEN=$( $VAULT_READ secret/dnsimple_token )
 echo "Getting dnsimple account id..."
-DNSIMPLE_ACCOUNT=$( $VAULT_READ secret/dnsimple_account | $JQ )
+DNSIMPLE_ACCOUNT=$( $VAULT_READ secret/dnsimple_account )
+echo "Getting bitbucket secret key..."
+BITBUCKET_RSA=$( $VAULT_READ secret/bitbucket_rsa )
 echo "Done! Executing terraform"
 
 $DOCKER_RUN $2  \
@@ -32,4 +32,6 @@ $DOCKER_RUN $2  \
   -var "registry_htpasswd='${REGISTRY_HTPASSWD}'" \
   -var "dnsimple_token=${DNSIMPLE_TOKEN}" \
   -var "dnsimple_account=${DNSIMPLE_ACCOUNT}" \
+  -var "bitbucket_rsa=${BITBUCKET_RSA}" \
+  -var "bitbucket_rsa_pub=${BITBUCKET_RSA_PUB}" \
   -var "agent_token=${BUILDKITE_AGENT_TOKEN}"
